bfc5f8701fb23d5cba7aee3de20219fb51e321f2
eminbozgeyik <eminbozgeyik@hotmail.com>
2022-03-12 15:02:34 +0300

    Templates were added

--- djangoprojem2022/base/views.py
+++ djangoprojem2022/base/views.py
@@ -1,10 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Home page')
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Lets learn C#!'},
+    {'id':3, 'name':'Lets learn JS!'},
+]
 
-def room(request):
-    return HttpResponse('Room')
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            print(i)
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
