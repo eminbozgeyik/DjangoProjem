b9375832890136df0dac5059bb5e2cafb0ac5ce1
eminbozgeyik <eminbozgeyik@hotmail.com>
2022-03-24 21:08:44 +0300

    forms

--- djangobackend/base/views.py
+++ djangobackend/base/views.py
@@ -1,7 +1,8 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -20,3 +21,14 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request, 'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
