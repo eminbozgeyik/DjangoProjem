25b08f2d872e34970d6318c657191b74caeca7f0
eminbozgeyik <eminbozgeyik@hotmail.com>
2022-03-26 21:51:56 +0300

    delete method

--- djangobackend/base/views.py
+++ djangobackend/base/views.py
@@ -1,4 +1,5 @@
 from multiprocessing import context
+from traceback import format_list
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
@@ -15,10 +16,12 @@ from .forms import RoomForm
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms':rooms}
+
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+
     context = {'room':room}
     return render(request, 'base/room.html',context)
 
@@ -32,3 +35,23 @@ def createRoom(request):
 
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request,'base/delete.html', {'obj':room})
